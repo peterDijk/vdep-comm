54a0518c136512b9302ba01df7e5f493d34f3109
Peter van Dijk <petervandijk@gmail.com>
2019-04-22 10:37:19 +0200

    fetch data from vdep-graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,34 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, StaticQuery, graphql } from "gatsby";
 import { ThemeProvider } from "styled-components";
 
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO
-      title="Home"
-      keywords={[`gatsby`, `application`, `react`]}
-      description="omschrijving"
-    />
-    <h1>Communicatie over Grenzen</h1>
-    <h2>subtitle</h2>
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  console.log(data);
+  return (
+    <Layout>
+      <SEO
+        title="Home"
+        keywords={[`gatsby`, `application`, `react`]}
+        description="omschrijving"
+      />
+      <h1>Communicatie over Grenzen</h1>
+      <h2>subtitle</h2>
+    </Layout>
+  );
+};
+
+export const queryName = graphql`
+  query {
+    drupal {
+      availableLanguages {
+        name
+      }
+    }
+  }
+`;
 
 export default IndexPage;
