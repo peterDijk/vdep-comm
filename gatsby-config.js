c6ed71878d23bb26017eab2e01cdd43022d0eee7
Peter van Dijk <petervandijk@gmail.com>
2019-05-01 22:52:32 +0200

    offline-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -53,8 +54,5 @@ module.exports = {
         prefixDefault: false,
       }
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
   ],
 }
