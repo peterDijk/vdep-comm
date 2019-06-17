f7b743b1f5acd5422ce297a18cde4ef386f7daa6
Peter van Dijk <petervandijk@gmail.com>
2019-06-17 22:01:58 +0200

    cookie consent temp text

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,11 +3,14 @@ import Grid from "@material-ui/core/Grid";
 import { Provider } from "../lib/Providers";
 import { NavBar, SeminarMenu } from "./NavBar";
 import { Global } from "../styles";
+import CookieConsent from "react-cookie-consent";
 
 import { StyledContainer } from "../styles/Main";
 
 import { loadReCaptcha } from "react-recaptcha-v3";
 import { Footer } from "./Footer";
+import { COLOR } from "../styles/constants";
+import { Button } from "../styles/buttons";
 
 type Props = {
   lang: string;
@@ -48,6 +51,31 @@ class Layout extends React.Component<Props, State> {
           <Grid container spacing={0}>
             <StyledContainer id="StyledContainer">
               <Grid item xs={12}>
+                <CookieConsent
+                  debug={true}
+                  location="bottom"
+                  buttonText="OK"
+                  cookieName="Communicatieovergrenzen.nl_consent"
+                  cookieValue={true}
+                  style={{ background: COLOR.VDEP_BLUE }}
+                  buttonStyle={{
+                    color: COLOR.VDEP_DARK_BLUE,
+                    fontSize: "2rem",
+                    background: COLOR.VDEP_GREEN,
+                    borderRadius: ".5rem",
+                    width: "10rem",
+                  }}
+                  ButtonComponent={Button}
+                  expires={14}
+                >
+                  <span style={{ fontSize: "1.4rem", textAlign: "center" }}>
+                    This site uses cookies and other tracking technologies to
+                    assist with navigation and your ability to provide feedback,
+                    analyse your use of our products and services, assist with
+                    our promotional and marketing efforts, and provide content
+                    from third parties. <a href="#">Cookie Policy </a>
+                  </span>
+                </CookieConsent>
                 <NavBar
                   language={lang}
                   switchBg={this.state.navWhite}
